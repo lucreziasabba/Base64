6bcd2b68f4179d0b1c0578310602eeca73cb7935
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-22 13:20:30 +0100

    add test

--- Base64___Tests/B64_Test.cs
+++ Base64___Tests/B64_Test.cs
@@ -10,7 +10,7 @@ namespace Base64___Tests
         public void Base64Test1()
         {
             string risposta = B64.Base64();
-            Assert.AreEqual("THVjcmV6aWE=", risposta);
+            Assert.AreEqual("THVjcmV6aWE=", risposta); 
         }
     }
 }
